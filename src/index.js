fcd3d644e30504dcaeb79fb106a3bbed42baffbb
Namibia Torres <namibialt@gmail.com>
2018-04-20 11:32:04 +1000

    add first class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ToDoList extends Component{
+  function toDo(props) {
+    const things = props.things;
+    const listItems = things.map((chore) =>
+      <li key={chore}>
+        {item}
+      </li>
+    );
+    return (
+      <ul>{listItems}</ul>
+    );
+  }
+}
+
+
+const things = ['react', 'buy groceries', 'pay bill'];
+
+
+ReactDOM.render(<App things={things}/>, document.getElementById('root'));
 registerServiceWorker();
