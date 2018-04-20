60f1bf7da3df6859c7c09ff60976518ffb3091a4
Namibia Torres <namibialt@gmail.com>
2018-04-20 14:52:45 +1000

    handle delete and add tasks

--- src/index.js
+++ src/index.js
@@ -1,29 +1,60 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import ToDoList from './ToDoList';
 
-class ToDoList extends Component{
+class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: ['Laundry', 'Grocery Shopping', 'Cook dinner'],
+    };
+  }
+  handleAdd(event) {
+    this.setState(previousState => ({
+      tasks: [...previousState.tasks, 'Have Amazon ship something'],
+    }));
+  }
+  handleDelete(deletedTask) {
+    var list = this.state.tasks;
+    this.setState({
+      tasks: list.filter(task => {
+        return task !== deletedTask;
+      }),
+    });
+  }
   render() {
-    return(
+    return (
       <div className="myList">
-        <div className="header">
-          <form>
-            <input placeholder="enter task">
-            </input>
-            <button type="submit">Add</button>
-            <button type="submit">Delete</button>
-          </form>
-        </div>
+        <h4> Tasks: </h4>
+        <ul>
+          {this.state.tasks.map(task => (
+            <li key={task}>
+              {task}
+              <button
+                className="deleteButton"
+                onClick={() => {
+                  this.handleDelete(task);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <input placeholder="enter task" />
+        <button className="addButton" onClick={this.handleAdd.bind(this)}>
+          Add
+        </button>
+        <button className="clearAllButton">Clear all</button>
       </div>
-    )
+    );
   }
 }
 
-
-const things = ['react', 'buy groceries', 'pay bill'];
-
-
-ReactDOM.render(<ToDoList/>, document.getElementById('root'));
+ReactDOM.render(<ToDoList />, document.getElementById('root'));
 registerServiceWorker();
